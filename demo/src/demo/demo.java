81cb25ae4db25bbc25a319caa234afce91bf139b
ragulmanoharan1994 <ragulmanoharan1994@gmail.com>
2022-02-06 13:53:47 +0530

    banch and Commit

--- demo/src/demo/demo.java
+++ demo/src/demo/demo.java
@@ -5,7 +5,10 @@ public class demo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Ragul");
-System.out.println("Jane");
+String a="Jane";
+String b="Deena";
+System.out.println(a+" "+b);
+
 	}
 
 }
